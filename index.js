85cbe85d7218a59ff28b03f0e1f3ecef6b2d05af
Pedro <voxmachina@gmail.com>
2018-06-05 17:35:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-module.exports = function(content) {
-  this.cacheable && this.cacheable();
-  this.value = content;
+export default (content, options) => {
+  this.cacheable && this.cacheable()
+  this.value = content
 
-  // Escape backticks and backslashes: “`” ⇒ “\`”, “\” ⇒ “\\”
-  var escapedContent = content.replace(/[`\\]/g, function(match) {
-    return "\\" + match;
-  });
+  const escapedContent = content.replace(/[`\\]/g, function(match) {
+    return '\\' + match
+  })
+  const type = options.type === 'global' || options.type === 'resolve' ? `.${options.type}` : ''
 
-  return "import css from 'styled-jsx/css';\n\nexport default css`" + escapedContent + "`;";
-};
+  return 'import css from \'styled-jsx/css\';\n\nexport default css'+type+'`' + escapedContent + '`;'
+}
